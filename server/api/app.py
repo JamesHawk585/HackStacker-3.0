bdbabd96ed0b4d1ff3e470d705782a4d45050641
James Hawk <jph94880@gmail.com>
2024-08-10 16:45:36 -0500

    Moves schema from app.py to schema.py

--- server/api/app.py
+++ server/api/app.py
@@ -10,98 +10,20 @@ from config import app, db, api, ma
 from models import User, BlogPost, Comment, Category, db
 from sqlalchemy.exc import IntegrityError
 from marshmallow import fields
+
+from api.cookies import cookies
+
 from api.iam import Signup
 from api.iam import CheckSession
 from api.iam import Login
 from api.iam import Logout
 
+from api.schema import UserSchema
+from api.schema import BlogPostSchema
+from api.schema import CommentSchema
+from api.schema import CategorySchema
 
 
-class UserSchema(ma.SQLAlchemySchema):
-    id = fields.Int(dump_only=True)
-
-    class Meta:
-        model = User
-    username = ma.auto_field()
-    bio = ma.auto_field()
-    blog_posts = ma.auto_field()
-    comments = ma.auto_field()
 
 
-    url = ma.Hyperlinks(
-        {
-            "self": ma.URLFor(
-                "user_by_id",
-                values=dict(id="<id>")),
-            "collection": ma.URLFor("users"),
-        }
-    )
 
-user_schema = UserSchema()
-users_schema = UserSchema(many=True)
-
-
-class BlogPostSchema(ma.SQLAlchemySchema):
-        id = fields.Int(dump_only=True)
-    
-        class Meta:
-            model = BlogPost
-    
-        title = ma.auto_field()
-        blog_content = ma.auto_field()
-        publication_date = ma.auto_field()
-        edited_at = ma.auto_field()
-        user_id = ma.auto_field()
-    
-        url = ma.Hyperlinks(
-            {
-                "self": ma.URLFor(
-                    "blog_post_by_id",
-                    values=dict(id="<id>")),
-                "collection": ma.URLFor("blog_posts"),
-            }
-        )
-
-blog_post_schema = BlogPostSchema()
-blog_posts_schema = BlogPostSchema(many=True)
-
-class CommentSchema(ma.SQLAlchemySchema):
-    id = fields.Int(dump_only=True)
-        
-    class Meta:
-        model = Comment
-        
-    comment_content = ma.auto_field()
-        
-    url = ma.Hyperlinks(
-        {
-            "self": ma.URLFor(
-                "comment_by_id",
-                values=dict(id="<id>")),
-            "collection": ma.URLFor("comments"),
-        }
-    )
-
-comment_schema = CommentSchema()
-comments_schema = CommentSchema(many=True)
-
-class CategorySchema(ma.SQLAlchemySchema):
-    id = fields.Int(dump_only=True)
-            
-    class Meta:
-        model = Category
-            
-    name = ma.auto_field()
-    description = ma.auto_field()
-            
-    url = ma.Hyperlinks(
-        {
-            "self": ma.URLFor(
-                "category_by_id",
-                values=dict(id="<id>")),
-            "collection": ma.URLFor("categories"),
-        }
-    )
-
-category_schema = CategorySchema()
-categories_schema = CategorySchema(many=True)
